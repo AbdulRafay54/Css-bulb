785627d18673ca769025682ffc1210b764fca662
AbdulRafay54 <139426616+AbdulRafay54@users.noreply.github.com>
2024-03-06 09:45:52 +0500

    Add files via upload

---
+++ bulb/app.js
@@ -0,0 +1,16 @@
+// function swapImage() {
+//     let img = document.querySelector("#bulb-off");
+//     img.src = "./img/bulb-on.jpg"
+
+// }
+// function backToOriginal() {
+//     let img = document.querySelector("#bulb-off");
+//     img.src = "./img/bulb-of.jpg"
+// }
+ 
+function anyColor(){
+    
+}
+
+ 
+
